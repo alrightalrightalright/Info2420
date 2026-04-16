9fe3185fa3b8ce056eed69391cbb0d7b9a15afea
alrightalrightalright <35151617+alrightalrightalright@users.noreply.github.com>
2026-04-15 23:11:50 -0600

    project 7

---
+++ Projects/Project7/myscripts.js
@@ -0,0 +1,49 @@
+// Preload print button and logo rollover images
+PrintButton1 = new Image;
+PrintButton2 = new Image;
+logo1 = new Image;
+logo2 = new Image;
+if (document.images) {
+	PrintButton1.src = "Images/Print1.jpg";
+	PrintButton2.src = "Images/Print2.jpg";
+	logo1.src = "Images/Logo1.jpg";
+	logo2.src = "Images/Logo2.jpg";
+}
+
+// Image swap arrays
+var imgArray = new Array(
+	'Image1Lg.jpg',
+	'Image2Lg.jpg',
+	'Image3Lg.jpg',
+	'Image4Lg.jpg'
+);
+
+var titleArray = new Array(
+	'Couple Sailing',
+	'Couple Hiking',
+	'Couple Biking',
+	'Couple Evening'
+);
+
+var imgPath = "Images/fullsize/";
+
+function swapImage(imgID) {
+	var theImage = document.getElementById('theImage');
+	var textDiv = document.getElementById('bottomText');
+
+	var newImg;
+	var textTitle;
+
+	newImg = imgArray[imgID];
+	theImage.src = imgPath + newImg;
+
+	textTitle = titleArray[imgID];
+	textDiv.innerHTML = textTitle;
+}
+
+function preloadImages() {
+	for (var i = 0; i < imgArray.length; i++) {
+		var tmpImg = new Image;
+		tmpImg.src = imgPath + imgArray[i];
+	}
+}
